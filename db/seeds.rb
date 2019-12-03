7e4388fc116efaa52fc4b43b43f90f9e816b5ac3
sashiiredev <alishiret@gmail.com>
2019-12-03 16:03:02 +0100

    seed working ... !

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,91 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |category|
+    Category.create(
+     name: Faker::Name.name,
+     logo: Faker::Company.industry
+         )
+    puts "Categories created"
+end
+10.times do |busnessline|
+    Businessline.create(
+     name: Faker::Name.name,
+     description: Faker::Company.type
+         )
+    puts "Busnessline created"
+end
+ 10.times do |company|
+     Company.create(
+      name: Faker::Name.name,
+      address: Faker::Address.street_address ,
+      logo: Faker::Cosmere.shard_world,
+      mobile: Faker::Number.leading_zero_number(digits: 10),
+      businessline: Businessline.find(rand(1..10))
+          )
+     puts "Company created"
+ end
+
+ 10.times do |branch|
+    Branch.create(
+     name:   Faker::Name.name,
+     address: Faker::Address.street_address ,
+     mobile: Faker::Alphanumeric.alphanumeric(number: 10),
+     company: Company.find(rand(1..10))
+         )
+    puts "Branch created"
+end
+
+10.times do |closedcashes|
+    Closedcash.create(
+     money:   Faker::Number.decimal(l_digits: 2),
+     host:    Faker::Name.name ,
+     branch: Branch.find(rand(1..10))
+         )
+
+    puts "ClosedCah created"
+end
+
+screens = [
+  "User",
+  "Create user",
+  "Roles",
+  "Screens",
+  "Permissions",
+  "Companies",
+  "Branch",
+  "Business Lines",
+  "Products",
+  "Categories",
+  "Suppliers",
+  "Customs",
+  "Stock Management",
+  "Purshas",
+  "Sales"
+]
+screens.each do |screen|
+  Screen.create(
+    name: screen,
+    url: 'url',
+  )
+end
+
+roles = ["admin","employee", "manager"]
+ roles.each do |role|
+    Role.create(
+      name: role,
+    )
+     p "Roles created"
+ end
+
+10.times do |paymentsuppliers|
+    Paymentsupplier.create(
+    paymentNote: Faker::Number.decimal(l_digits: 2),
+    paymentValue: Faker::Number.decimal(l_digits: 2)
+
+    )
+    puts "Paymentsupplier created"
+end
