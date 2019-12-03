947485f214312f4d4c13db51d3293ed78f10122e
walid <wmersaoui@gmail.com>
2019-12-03 15:39:42 +0100

    supprimer la table employee

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,6 @@
 class Ticket < ApplicationRecord
     belongs_to :branch
-    has_many :employees
+    has_many :users
     has_many :customers
     has_many :ticketlines
     belongs_to :receipt
--- db/migrate/20191202102702_devise_create_users.rb
+++ db/migrate/20191202102702_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :last_name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.boolean :is_admin, default: true
+      t.belongs_to :role
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
