d5cfdfdb856f306a3a886c50b29441cee4fd6950
walid <wmersaoui@gmail.com>
2019-12-05 17:44:43 +0100

    conflic

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_12_02_115455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "apppassword"
+    t.string "address"
+    t.string "email"
+    t.string "image"
+    t.bigint "company_id"
+    t.bigint "role_id"
+    t.bigint "ticket_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_employees_on_company_id"
+    t.index ["role_id"], name: "index_employees_on_role_id"
+    t.index ["ticket_id"], name: "index_employees_on_ticket_id"
+  end
 
 end
