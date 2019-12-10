878ccf59db78bbae78c35271c95582c3cf6faa13
sarra <sarra.bouras1@gmail.com>
2019-12-10 23:39:06 +0100

    customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,5 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
-  skip_before_action :verify_authenticity_token, raise: false
 
   # GET /customers
   # GET /customers.json
@@ -25,29 +24,31 @@ class CustomersController < ApplicationController
   # POST /customers
   # POST /customers.json
   def create
-    Customer.create(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      adress: params[:adress],
-      email: params[:email],
-      mobile: params[:mobile], 
-    )
-    redirect_to customers_path
+    @customer = Customer.new(customer_params)
+
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
+        format.json { render :show, status: :created, location: @customer }
+      else
+        format.html { render :new }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PATCH/PUT /customers/1
   # PATCH/PUT /customers/1.json
   def update
-    @customer = Customer.find(params[:id])
-    @customer.update(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      adress: params[:adress],
-      email: params[:email],
-      mobile: params[:mobile]
-    )
-    redirect_to customers_path
-    flash[:notice] = "Customer edited"
+    respond_to do |format|
+      if @customer.update(customer_params)
+        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.json { render :show, status: :ok, location: @customer }
+      else
+        format.html { render :edit }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /customers/1
@@ -63,11 +64,11 @@ class CustomersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_customer
-     @customer = Customer.find(params[:id])
+      @customer = Customer.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-  #params.require(:customer).permit(:first_name, :last_name, :adress, :email, :mobile)
+      params.require(:customer).permit(:first_name, :last_name, :adress, :email, :mobile)
     end
 end
