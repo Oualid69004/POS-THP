44576529ae48e40e50c6508c2fa63a3f771bfd7a
llensi <lucas.lensi@gmail.com>
2019-12-11 17:05:11 +0100

    create user complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :welcome_send , :welcome_send_employee, :user_stock
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,15 +11,23 @@ class User < ApplicationRecord
          belongs_to :memory, optional: true
          belongs_to :memory_sale, optional: true
 
-  after_create :welcome_send , :welcome_send_employee
+  private
 
   def welcome_send
    UserMailer.welcome_email(self).deliver_now
   end
 
-
   def welcome_send_employee
    UserMailer.welcome_employee(self).deliver_now
+  end
+
+  def user_stock
+    memory = Memory.create(user: self)
+    memory_sales = MemorySale.create(user: self)
+    self.update(
+      memory: memory,
+      memory_sale: memory_sales
+    )
   end
 
 end
