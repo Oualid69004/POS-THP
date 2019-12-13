21fbccce5473ddca3ca373bdf615ab6ce4865c8d
llensi <lucas.lensi@gmail.com>
2019-12-13 03:54:21 +0100

    stripe ok

--- app/controllers/ticketlines_controller.rb
+++ app/controllers/ticketlines_controller.rb
@@ -25,11 +25,27 @@ class TicketlinesController < ApplicationController
   # POST /ticketlines.json
   def create
 
-    @amount = 500
+    @amount = params[:price].to_i * 100
+
+  customer = Stripe::Customer.create({
+    email: params[:stripeEmail],
+    source: params[:stripeToken],
+  })
+
+  charge = Stripe::Charge.create({
+    customer: customer.id,
+    amount: @amount,
+    description: 'Rails Stripe customer',
+    currency: 'eur',
+  })
 
     Ticket.purshas(current_user)
     flash[:success] = 'Votre commande a bien été prit en compte'
     redirect_to purshas_path
+
+  rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
 
   end
 
